766cd4ff71e4ea60b4366a7a07e6dec84ba439f3
Sadiqur199 <sadiqurrhaman199@gmail.com>
2023-04-27 03:08:31 +0600

    news api connected

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const cors = require('cors')
 const port = process.env.port ||5000;
+
 const catagories = require('./Data/catagories.json')
+const news = require('./Data/news.json')
 
 app.use(cors())
 
@@ -14,6 +16,32 @@ app.get('/',(req,res)=>{
 app.get('/catagories',(req,res)=>{
   res.send(catagories)
 })
+//All News Here
+app.get('/news',(req,res)=>{
+  res.send(news)
+})
+
+//specific News
+app.get('/news/:id',(req,res)=>{
+const id = req.params.id
+console.log(id)
+const selectedNews = news.find(n=>n._id === id)
+res.send(selectedNews);
+})
+
+//Category News 
+app.get('/catagories/:id',(req,res)=>{
+  const id = parseInt(req.params.id)
+  console.log(id)
+  if(id===0){
+    res.send(news)
+  }
+  else{
+  const categoryNews = news.filter(n=> parseInt(n.category_id) === id)
+  res.send(categoryNews)
+}
+})
+
 
 app.listen(port,()=>{
   console.log(`Dragon Api Is Running ${port}`)
